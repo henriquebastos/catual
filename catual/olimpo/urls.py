7c7cb82aa2d803c1cdd4b86f8d3811d01f6e6f4f
Marcus Gabaldo <gabaldo@gmail.com>
2016-02-09 22:48:17 -0200

    refatorando login

--- catual/olimpo/urls.py
+++ catual/olimpo/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
-from catual.olimpo.views import ocorrencias, painel, eventos
-from django.contrib.auth.views import login, logout
+from catual.olimpo.views import ocorrencias, painel, eventos, login
+from django.contrib.auth.views import logout
 
 urlpatterns = [
     url(r'^$', painel, name='painel'),
     url(r'^ocorrencias/$', ocorrencias, name='ocorrencias'),
     url(r'^eventos/$', eventos, name='eventos'),
-    url(r'^entrar/$', login, {'template_name': 'olimpo/login.html'}, name='login'),
+    #url(r'^entrar/$', login, {'template_name': 'olimpo/login.html'}, name='login'),
     url(r'^sair/$', logout, {'next_page': 'home'}, name='logout'),
+    url(r'^login/$', login,  name='login'),
 ]
--- catual/olimpo/views.py
+++ catual/olimpo/views.py
@@ -1,6 +1,26 @@
+from django.contrib.auth import (login as auth_login, authenticate)
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+
+def login(request):
+    _message = 'Entre com seus dados'
+    if request.method == 'POST':
+        _username = request.POST['username']
+        _password = request.POST['password']
+        user = authenticate(username=_username, password=_password)
+        if user is not None:
+            if user.is_active:
+                auth_login(request, user)
+                return HttpResponseRedirect(reverse('olimpo:painel'))
+            else:
+                _message = 'Sua conta não está ativada'
+        else:
+            _message = 'Login inválido, tente novamente'
+    context = {'message': _message}
+    return render(request, 'olimpo/login_olimpo_form.html', context)
 
 @login_required
 def painel(request):
@@ -13,4 +33,3 @@ def ocorrencias(request):
 @login_required
 def eventos(request):
     return render(request, 'olimpo/eventos.html')
-
